d5e0011d22802fa6c1d552cca18f6edf9b25da9a
Jiang <jiangyue0380@gmail.com>
2022-10-28 19:39:13 +1100

    added comments to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import DisplayMusic from "./components/DisplayMusic/DisplayMusic";
 import AddANewSong from "./components/AddANewSong/AddANewSong";
 import SearchSong from "./components/SearchSong/SearchSong";
 function App() {
 
+  // delcatre state variable song to hold all the songs from database
   const [songs, setSongs] = useState([]);
+  
   //displays all the songs in the database when the page first render 
   useEffect(() => {
     getAllSongs();
@@ -14,15 +16,18 @@ function App() {
   async function getAllSongs() {
     // when the page render the first time use axios.get to retrive all songs stored in the database
     const response = await axios.get(`http://127.0.0.1:8000/api/music/`);
+    // setSongs will updated the state variable songs to response.data
     setSongs(response.data);
   }
 
   async function addSong(newSongInfo) {
     // axios.post to update the database with song's information submitted by AddANewSong component
     await axios.post('http://127.0.0.1:8000/api/music/', newSongInfo);
+    // when a new song added to the database, call getAllSongs again to display the recent updated song to the page
     getAllSongs();
   }
 
+  // decalare a state variable searchType to hold the 
   const [searchType, setSearchType] = useState('')
   async function searchResult(searchValue, serachType) {
     setSearchType(serachType);
@@ -40,7 +45,7 @@ function App() {
     
     }
     else {
-      // console.log(`http://127.0.0.1:8000/api/music/?${serachType}=${searchValue}`)
+
       try {
         const response = await axios.get(`http://127.0.0.1:8000/api/music/?${serachType}=${searchValue}`);
         setSongs(response.data);
@@ -53,14 +58,13 @@ function App() {
     }
   }
 
-  // allow user to choose search for songs, add a new song, update a song
+  // toggle SearchSong and AddANewSong buttont
   const [showSearchSong, setShowSearchSong] = useState(false)
   const [showAddSong, setAddSong] = useState(false)
   function chooseSearchSong() {
     setShowSearchSong(!showSearchSong);
     setAddSong(false);
   }
-
   function chooseAddSong() {
     setShowSearchSong(false);
     setAddSong(!showAddSong);
@@ -68,7 +72,6 @@ function App() {
 
   return (
     <div>
-      <p>What would you like to do with you music library?</p>
       <button type="button" onClick={() => chooseSearchSong()}>Search for a song</button>
       <button type="button" onClick={() => chooseAddSong()}>Add a new song </button>
 
